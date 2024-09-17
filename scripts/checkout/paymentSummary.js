ccfebcd975e6fe59e32996fe6b7572ceb3e25f5b
Davit-M <87268065+davitm7@users.noreply.github.com>
2024-09-17 16:53:05 +0400

    fix some bugs

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -7,9 +7,11 @@ export function renderPaymentSummary(){
 
     let  productPriceCents = 0;
     let shippingPriceCents = 0;
+    let productQuantity = 0;
 
     cart.forEach((cartItem) => {
         const product = getProduct(cartItem.productId);
+        productQuantity += cartItem.quantity;
         productPriceCents += product.priceCents * cartItem.quantity;
 
         const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);  
@@ -28,7 +30,7 @@ export function renderPaymentSummary(){
           </div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items (${productQuantity}):</div>
             <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
           </div>
 
@@ -62,6 +64,7 @@ export function renderPaymentSummary(){
         .innerHTML = paymentSummaryHtml;
         
     
-    
+    document.querySelector('.js-product-quantity')
+    .innerHTML = productQuantity;    
     
 }
